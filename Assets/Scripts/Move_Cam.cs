0aa757922fc8ec5be78f43c7092eac1969ab8c79
Stumpkin <banksjalen0@gmail.com>
2020-02-19 13:25:27 -0800

    Most of Part 1
    intergrated level parser
    GUI with a working timer
    textured blocks
    prefabs

---
+++ Assets/Scripts/Move_Cam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Cam : MonoBehaviour
+{
+    public float speed = .25f;
+    Vector3 move2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move2D = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        transform.Translate(move2D);
+
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    private TextMeshProUGUI clock;
+    private float intal = 400f;
+    private float remain = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clock = GetComponent<TextMeshProUGUI>();
+        clock.text = intal.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        intal -= 1 * Time.deltaTime;
+        int con = (int)intal;
+        clock.text = con.ToString();
+
+
+    }
+}
